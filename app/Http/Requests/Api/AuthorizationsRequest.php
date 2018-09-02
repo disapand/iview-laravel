4c69d8e29fcbff5a9dc1391270f6686c76bfcfde
deced <shadow724156286@hotmail.com>
2018-09-02 18:48:23 +0800

    删除唯一性验证

--- app/Http/Requests/Api/AuthorizationsRequest.php
+++ app/Http/Requests/Api/AuthorizationsRequest.php
@@ -24,7 +24,7 @@ class AuthorizationsRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string|unique:users,name',
+            'name' => 'required|string',
             'password' => 'required|string|min:6'
         ];
     }
