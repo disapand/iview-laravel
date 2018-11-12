e99f0586eda1efd2b070f7e5be3a43fd461d58e5
deced <shadow724156286@hotmail.com>
2018-11-12 20:25:03 +0800

    修改了编辑器的连接

--- resources/assets/js/components/editor.vue
+++ resources/assets/js/components/editor.vue
@@ -45,7 +45,7 @@
             },
             serverUrl: {
                 type: String,
-                default: 'http://www.zetin.cn/api/otherImg',
+                default: 'https://www.zetin.cn/api/otherImg',
             }
         },
         data() {
